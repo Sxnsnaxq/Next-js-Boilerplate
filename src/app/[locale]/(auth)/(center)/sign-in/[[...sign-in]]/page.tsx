d7321e6edf79b023b3094dc465fa272b307346bd
Suriya Suksawat <3suriya.3s@gmail.com>
2024-11-25 17:28:19 +0700

    Update page.tsx

--- src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
@@ -9,7 +9,7 @@ export default function SignInPage() {
       <PantipHeader />
       <div className="flex h-screen w-screen items-center justify-center bg-purple-900 text-white">
         <div className="mx-auto w-full max-w-4xl p-4">
-          {/* หัวข้อ */}
+
           <div className="mb-6 text-center">
             <h1 className="text-2xl font-semibold text-purple-300">
               ลงชื่อเข้าใช้งาน
@@ -19,9 +19,7 @@ export default function SignInPage() {
             </h2>
           </div>
 
-          {/* คอนเทนต์หลัก */}
           <div className="flex">
-            {/* ด้านซ้าย */}
             <div>
               <div className="space-y-3">
                 <input
@@ -37,7 +35,6 @@ export default function SignInPage() {
                   className="w-full rounded border-none bg-purple-700 px-4 py-2 text-white placeholder:text-purple-400 focus:outline-none"
                 />
 
-                {/* ลิงก์ลืมรหัสผ่าน */}
                 <div className="text-right">
                   <a href="/" className="text-purple-300 hover:underline">
                     ลืมรหัสผ่าน
@@ -45,25 +42,19 @@ export default function SignInPage() {
                 </div>
               </div>
 
-              {/* ปุ่มเข้าสู่ระบบ */}
               <button className="mt-4 w-full rounded bg-purple-600 py-2 text-white hover:bg-purple-700">
                 เข้าสู่ระบบ
               </button>
             </div>
 
-            {/* ตัวแบ่งด้วยคำว่า 'หรือ' และเส้นคั่น */}
             <div className="relative flex items-center justify-center self-stretch px-4">
-              {/* เส้นคั่นแนวตั้ง */}
               <div className="absolute h-full w-px bg-purple-500"></div>
-              {/* คำว่า 'หรือ' */}
               <span className="z-10 bg-purple-900 px-2 text-purple-300">หรือ</span>
             </div>
 
-            {/* ด้านขวา */}
             <div className="w-1/2 pl-4">
-              {/* ปุ่มเข้าสู่ระบบด้วยโซเชียลมีเดีย */}
               <div className="space-y-3">
-                {/* Facebook */}
+
                 <a
                   href="https://www.facebook.com/?stype=lo&flo=1&deoia=1&jlou=AffeVSwdd1wV2-y_ubHueYh4EYX4Nu73DE5zJbmS-0Xjs4XfE4o0Ic2b4dN3VOUwkr24755YWhqPIK976b0wDR_fOBHtvm6r5NgSYOK5ihf8FQ&smuh=5885&lh=Ac9w0xNSJutgpPnjVeY"
                   target="_blank"
@@ -73,7 +64,6 @@ export default function SignInPage() {
                   <span>ดำเนินการต่อด้วย Facebook</span>
                 </a>
 
-                {/* Google */}
                 <a
                   href="https://accounts.google.com/InteractiveLogin/signinchooser?elo=1&ifkv=AcMMx-eTau-bi-YJvqteXP_-Mi2Rjebqz3dYhzq_NaaY0-IA_wwdtx6sUxyugDNm_ipGaDPfgjeCtg&ddm=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
                   target="_blank"
@@ -83,7 +73,6 @@ export default function SignInPage() {
                   <span>ดำเนินการต่อด้วย Google</span>
                 </a>
 
-                {/* LINE */}
                 <a
                   href="https://access.line.me/oauth2/v2.1/login?fromDomain=access-auto.line.me&loginState=nBeMaOY7Mdu5aFeSo1hVfn&line_auto_login_error=universal_link_error&loginChannelId=1445834080&returnUri=oauth2v2.1authorizeconsentbot_promptnormalscopeopenidprofileresponse_typecodestateRhiVgBoLDGredirect_urihttps3A2F2Fform.tonariwa.com2Fsuntory2Fmeasure_url3Fcpid3Dtennensui_sampling426lcd3D126type3D126nonceEvutxGZ894client_id1445834080#/"
                   target="_blank"
@@ -93,7 +82,6 @@ export default function SignInPage() {
                   <span>ดำเนินการต่อด้วย LINE</span>
                 </a>
 
-                {/* Apple */}
                 <a
                   href="https://account.apple.com/sign-in"
                   target="_blank"
@@ -104,7 +92,6 @@ export default function SignInPage() {
                 </a>
               </div>
 
-              {/* ลิงก์สมัครสมาชิก */}
               <div className="mt-6 text-center text-sm">
                 ยังไม่เป็นสมาชิก?
                 {' '}
